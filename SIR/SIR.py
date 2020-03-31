239e39fa6f785a2fcc17ce2e7252f8a7b7d89592
antelk <alojic00@fesb.hr>
2020-04-01 00:44:32 +0200

    SIR finished

--- SIR/SIR.py
+++ SIR/SIR.py
@@ -1,71 +1,36 @@
 import numpy as np 
+from scipy.integrate import solve_ivp
+from scipy.optimize import minimize
+from sklearn.metrics import mean_squared_error
 
-class SIR(object):
-    """SIR disease model
+def RMSE(true_vals, preds):
+    return np.sqrt(mean_squared_error(true_vals, preds))
 
-    S' = - \beta * S * I
-    I' = \beta * S * I - \nu * I 
-    R' = \nu * I
-    """
-    def __init__(self, beta, nu, S0, I0, R0):
-        if isinstance(nu, (float, int)):
-            self.nu = lambda t: nu 
-        elif callable(nu, (float, int)):
-            self.nu = nu 
-        
-        if isinstance(nu, (float, int)):
-            self.beta = lambda t: nu 
-        elif callable(beta, (float, int)):
-            self.beta = beta 
+def loss(params, confirmed_cases, y0):
+    def SIR(t, y):
+        S, I, R = y
+        return [-beta*S*I, beta*S*I-gamma*I, gamma*I]
+    beta, gamma = params
+    size = len(confirmed_cases)
+    sol = solve_ivp(SIR, (0, size), y0, method='RK45', t_eval=np.arange(0, size, 1), vectorized=True)
+    return RMSE(sol.y[1], confirmed_cases)
 
-        self.initial_conds = [S0, I0, R0]
+class SIRModel(object):
+    def __init__(self):
+        pass
 
-    def __call__(self, u, t):
-        S, I, _ = u 
-        return np.asarray([
-            -self.beta(t) * S * I,
-            self.beta(t) * S * I - self.nu(t) * I,
-            self.nu(t) * I
-        ])
+    def fit(self, confirmed_cases, initial_conditions):
+        self.y0 = initial_conditions
+        opt = minimize(loss, [0.001, 0.001],
+                    args=(confirmed_cases, self.y0),
+                    method='L-BFGS-B', 
+                    bounds=[(0.00000001, 0.4), (0.00000001, 0.4)])
+        self.beta, self.gamma = opt.x 
+        return self.beta, self.gamma
 
-class solver:
-    """ODEsolver superclass
-
-    Any classes inheriting from this superclass must implement
-    advanc() method.
-    """
-    def __init__(self, f):
-        self.f = f 
-
-    def advance(self):
-        raise NotImplementedError
-
-    def set_initial_conds(self, U0):
-        if isinstance(U0, (int, float)):
-            # Scalaer ODE
-            self.number_of_equations = 1
-            U0 = float(U0)
-        else:
-            # System of ODEs
-            U0 = np.asarray(U0)
-            self.number_of_equations = U0.size 
-        self.U0 = U0
-
-    def solve(self, time_points):
-        self.t = np.asarray(time_points)
-        n = self.t.size 
-        self.u = np.empty((n, self.number_of_equations))
-        self.u[0, :] = self.U0
-
-        # integrate 
-        for i in range(n-1):
-            self.i = i 
-            self.u[i+1] = self.advance()
-
-        return self.u[:i+2], self.t[:i+2]
-
-class ForwardEuler(solver):
-    def advance(self):
-        u, f, i, t = self.u, self.f, self.i, self.t 
-        dt = t[i+1] - t[i]
-        return u[i, :] + dt * f(u[i, :], t[i]) 
+    def predict(self, n_days):
+        def SIR(t, y):
+            S, I, R = y
+            return [-self.beta*S*I, self.beta*S*I-self.gamma*I, self.gamma*I]
+        sol = solve_ivp(SIR, (0, n_days), self.y0, method='RK45', t_eval=np.arange(0, n_days, 1), vectorized=True)
+        return sol
