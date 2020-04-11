1ff3d0f8a35bfd62577fb6c083b52070d5ad2bf5
antelk <alojic00@fesb.hr>
2020-04-11 19:46:10 +0200

    R0 validation

--- run.py
+++ run.py
@@ -208,7 +208,7 @@ def seir_model(S0, E0, I0, R0, confirmed_cases, recovered_cases, split_ratio, ep
 
     seir_model = SEIR()
     beta, delta, alpha, gamma = seir_model.fit(train_confirmed_cases, train_recovered_cases, initial_conditions)
-    print(f'R0 = {beta/(delta+gamma)}')
+    R0 = beta/(delta+gamma)
     sol = seir_model.predict(n_future_days)
 
     end = epidemics_start_date + dt.timedelta(days=n_future_days)    
@@ -219,35 +219,35 @@ def seir_model(S0, E0, I0, R0, confirmed_cases, recovered_cases, split_ratio, ep
     _ = fig.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
     _ = fig.gca().xaxis.set_major_locator(mdates.DayLocator(interval=20))
 
-    #ax.plot(days, sol.y[0], 'k-', label='S(t)')
-    ax.plot(days, sol.y[1], 'b-', label='exposed')
+    #ax.plot(days, sol.y[0], 'k-', label='susceptible')
+    #ax.plot(days, sol.y[1], 'b-', label='exposed')
     ax.plot(days, sol.y[2], 'r-', label='infected')
     ax.plot(days, sol.y[3], 'g-', label='recovered/diceased')
     ax.plot(days[:len(train_confirmed_cases)],
             train_confirmed_cases, 
             linestyle='None', marker='x', color='red', alpha=0.7, 
-            label='train confirmed cases')
+            label='fitted confirmed')
     ax.plot(days[len(train_confirmed_cases):len(train_confirmed_cases)+len(test_confirmed_cases)], 
             test_confirmed_cases, 
             'ro', linestyle='None', 
-            label='test confirmed cases')
+            label='test confirmed')
     ax.plot(days[:len(train_recovered_cases)],
             train_recovered_cases, 
             linestyle='None', marker='x', color='green', alpha=0.7, 
-            label='train recovered cases')
+            label='fitted removed')
     ax.plot(days[len(train_recovered_cases):len(train_recovered_cases)+len(test_recovered_cases)], 
             test_recovered_cases, 
             'go', linestyle='None', 
-            label='test recovered cases')
+            label='test removed')
 
     _ = plt.gcf().autofmt_xdate()
     plt.ylabel('number of people')
 
     plt.grid()
     plt.legend(loc='upper right')
-    fig.savefig(f'figs/seir.pdf', bbox_inches='tight')
+    fig.savefig(f'figs/seir-{split_ratio}-split-ratio.pdf', bbox_inches='tight')
     plt.show()
-
+    return R0
 
 def main():
     latexconfig()
@@ -259,34 +259,41 @@ def main():
     death_cases = np.loadtxt('data/cro/death_cases.dat')
     removed_cases = recovered_cases + death_cases
     
-    ratio = 1
-    train_confirmed_cases, test_confirmed_cases = train_test_split(confirmed_cases, ratio)
-    train_recovered_cases, test_recovered_cases = train_test_split(recovered_cases, ratio)
+    # ratio = 1
+    # train_confirmed_cases, test_confirmed_cases = train_test_split(confirmed_cases, ratio)
+    # train_removed_cases, test_removed_cases = train_test_split(removed_cases, ratio)
     
-    # days since first case
-    x = np.arange(len(train_confirmed_cases))
+    # # days since first case
+    # x = np.arange(len(train_confirmed_cases))
 
-    # exp fit
-    exp_fit(x, train_confirmed_cases, test_confirmed_cases, n_future_days=7)
+    # # exp fit
+    # exp_fit(x, train_confirmed_cases, test_confirmed_cases, n_future_days=7)
 
-    # logit fit 
-    logit_fit(x, train_confirmed_cases, test_confirmed_cases, n_future_days=7)
+    # # logit fit 
+    # logit_fit(x, train_confirmed_cases, test_confirmed_cases, n_future_days=7)
 
-    # new cases averaged 
-    new_cases_plot(confirmed_cases, n_avg=7)
+    # # new cases averaged 
+    # new_cases_plot(confirmed_cases, n_avg=7)
 
-    # new cases v total cases averaged
-    averaged_new_cases_v_total_cases(confirmed_cases, period=7)
+    # # new cases v total cases averaged
+    # averaged_new_cases_v_total_cases(confirmed_cases, period=7)
 
     # susceptible-exposed-infected-recovered model
-    seir_model(S0=5000, 
-               E0=0, 
-               I0=confirmed_cases[0], 
-               R0=recovered_cases[0], 
-               confirmed_cases=confirmed_cases, 
-               recovered_cases=recovered_cases, 
-               split_ratio=0.8, 
-               epidemics_start_date=start_date)
+    split_ratio = [0.88, 0.96, 1.0]
+    R0 = np.empty(shape=(2, len(split_ratio)))
+    R0[0, :] = np.array(split_ratio)
+    for i, ratio in enumerate(split_ratio):
+        R0[1, i] = (seir_model(S0=5000, 
+                               E0=0, 
+                               I0=confirmed_cases[0], 
+                               R0=recovered_cases[0], 
+                               confirmed_cases=confirmed_cases, 
+                               recovered_cases=recovered_cases, 
+                               split_ratio=ratio, 
+                               epidemics_start_date=start_date))
+
+    file_name = f'params/{dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.txt'
+    np.savetxt(file_name, R0)
     
 if __name__ == "__main__":
     main()
