6418200c00f2063ceb7827ac4416eb3791a76840
antelk <alojic00@fesb.hr>
2020-08-31 15:13:52 +0200

    TO-DO: implement sensitivity analysis

--- covid_19/compartmental_models.py
+++ covid_19/compartmental_models.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.integrate import solve_ivp
 from scipy.optimize import minimize
 
-from covid_19.utils import mse, rmse, msle, mae
+from covid_19.utils import (mse, rmse, msle, mae)
 
 
 __all__ = ['SEIRModel', 'SEIRDModel']
@@ -10,7 +10,7 @@ __all__ = ['SEIRModel', 'SEIRDModel']
 
 def _SEIR(t, y, beta, delta, alpha, gamma):
     """Return the SEIR compartmental system values. For details check: 
-    https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology#The_SEIR_model
+    https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology
     
     Parameters
     ----------
@@ -23,7 +23,7 @@ def _SEIR(t, y, beta, delta, alpha, gamma):
     delta : float
         Direct transition rate between S and E individual. 
     alpha : float
-        Incubation rate, the reciprocal value of the incubation period. 
+        Incubation rate, the reciprocal value of the incubation period.
     gamma : float
         Recovery (or mortality) rate.
         
@@ -42,8 +42,8 @@ def _SEIR(t, y, beta, delta, alpha, gamma):
     ]
 
 def _SEIRD(t, y, beta, alpha, gamma, mu):
-    """Return the SEIRD compartmental system values. For details check: 
-    https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology#The_SIRD_model
+    """Return the SEIRD compartmental system values. For details check:
+    https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology
     
     Parameters
     ----------
@@ -54,7 +54,7 @@ def _SEIRD(t, y, beta, alpha, gamma, mu):
     beta : float
         Transition (infectious) rate controls the rate of spread. 
     alpha : float
-        Incubation rate, the reciprocal value of the incubation period. 
+        Incubation rate, the reciprocal value of the incubation period.
     gamma : float
         Recovery rate.
     mu : float
@@ -75,12 +75,81 @@ def _SEIRD(t, y, beta, alpha, gamma, mu):
         mu*I,
     ]
     
+class CompartmentalModel(object):
+    """General compartmental model class."""
 
-class SEIRModel(object):
+    def __init__(self, loss_fn='mse', sensitivity=None):
+        """Constructor.
+        
+        Parameters
+        ----------
+        loss_fn: str, optional
+            Loss function is `mse` by default
+        sensitivity : float, optional
+            Measure of the proportion of positives that are correctly
+            identified (e.g., the percentage of sick people who are
+            correctly identified as being infected). If the
+            `sensitivity` is defined, the .95 confidence intervals will
+            be caluclated.  
+        """
+        if loss_fn == 'mse':
+            self.loss_fn = mse
+        elif loss_fn == 'rmse':
+            self.loss_fn = rmse
+        elif loss_fn == 'msle':
+            self.loss_fn = msle
+        elif loss_fn == 'mae':
+            self.loss_fn = mae
+        else:
+            raise ValueError('Given loss function is not supported.')
+        if sensitivity:
+            assert isinstance(sensitivity, (float, )), \
+                'Sensitivity must be a floating point number in [0, 1]'
+            self.sensitivity = sensitivity
+    
+    @staticmethod
+    def calculate_ci(sensitivity, data, fitted):
+        """Return 2-d array with 3 rows, first row is the lower
+        confidence interval bound, the second row is the fitted data
+        and the last row is the upper confidence interval bound.
+
+        Parameters
+        ----------
+        sensitivity : float or numpy.ndarray
+            Test sensitivity. If the sensitivity is different in
+            different intervals, it should be stored in the array-like
+            format where the length is the same as the length of the
+            data.
+
+        data : numpy.ndarray
+            Original data on which the fitting was performed. 
+        fitted : numpy.ndarray
+            Fitted data.
+        """
+        std_sensitivity_err = np.sqrt(
+            np.divide(
+                (1 - sensitivity) * sensitivity, 
+                data, 
+                out=np.zeros_like(data), 
+                where=data!=0,
+                )
+            )
+        sensitivity_ci = 1.960 * std_sensitivity_err
+        lower_bound = (sensitivity - sensitivity_ci) * fitted
+        upper_bound = (sensitivity + sensitivity_ci) * fitted
+        return np.r_[
+            lower_bound.reshape(1, -1), 
+            fitted.reshape(1, -1), 
+            upper_bound.reshape(1, -1),
+            ]
+            
+
+class SEIRModel(CompartmentalModel):
     """SEIR model class."""
+
     def __init__(self):
         """Constructor."""
-        pass
+        super().__init__()
 
     def fit(
         self, 
@@ -88,27 +157,26 @@ class SEIRModel(object):
         removed_cases,
         initial_conditions,
         initial_guess=[0.001, 0.001, 0.001, 0.001],
-        loss_fn='mse'
-    ):
+        ):
         """Fit SEIR model.
         
         Parameters
         ----------
-        active_cases: numpy.ndarray
+        active_cases : numpy.ndarray
             Time series of currently active infected individuals.
-        removed_cases: numpy.ndarray
+        removed_cases : numpy.ndarray
             Time series of recovered+deceased individuals.
-        initial_conditions: list
+        initial_conditions : list
             Values of S, E, I and R at the first day.
         initial_guess : list, optional
-            Array of real elements by means of possible values of independent variables.
-        loss_fn : str, optional
-            Loss function is `mse` by default.
+            Array of real elements by means of possible values of
+            independent variables.
         
         Returns
         -------
         tuple
-            Fitted epidemiological parameters: beta, delta, alpha and gamma rate.
+            Fitted epidemiological parameters: beta, delta, alpha and
+            gamma rate.
         list
             Loss values during the optimization procedure.
         """
@@ -116,14 +184,20 @@ class SEIRModel(object):
         def print_loss(p):
             """Optimizer callback."""
             loss.append(
-                SEIRModel._loss(p, active_cases, removed_cases, initial_conditions)
+                SEIRModel._loss(
+                    p, 
+                    active_cases, 
+                    removed_cases, 
+                    initial_conditions, 
+                    self.loss_fn,
+                    )
             )
             
         self.y0 = initial_conditions
         opt = minimize(
             fun=SEIRModel._loss, 
             x0=initial_guess,
-            args=(active_cases, removed_cases, self.y0, loss_fn),
+            args=(active_cases, removed_cases, self.y0, self.loss_fn),
             method='L-BFGS-B',
             bounds=[(1e-5, 1.0), (1e-5, 1.0), (1e-5, 1.0), (1e-5, 1.0),],
             options={'maxiter': 1000, 'disp': True},
@@ -133,17 +207,18 @@ class SEIRModel(object):
         return (self.beta, self.delta, self.alpha, self.gamma), loss
 
     def predict(self, n_days):
-        """Forecast S, E, I and R based on the fitted epidemiological parameters.
+        """Forecast S, E, I and R based on the fitted epidemiological
+        parameters.
         
         Parameters
         ----------
         n_days : int
-            Number of days in future.
+            Number of days for which the simulation will be performed.
             
         Returns
         -------
         tuple
-            S, E, I and R values forecast for n_days in future.
+            S, E, I and R simulated values.
         """
         sol = solve_ivp(
             fun=_SEIR, 
@@ -154,11 +229,17 @@ class SEIRModel(object):
             t_eval=np.arange(0, n_days, 1), 
             vectorized=True,
         )
+        # TO-DO: implement sensitivity analysis
+        if self.sensitivity:
+            pass
         return (sol.y[0], sol.y[1], sol.y[2], sol.y[3])
     
     @staticmethod
-    def _loss(params, active_cases, removed_cases, initial_conditions, loss_fn='mse'):
-        """Calculate and return the loss function between actual and predicted values.
+    def _loss(
+        params, active_cases, removed_cases, initial_conditions, loss_fn
+        ):
+        """Calculate and return the loss function between actual and
+        predicted values.
         
         Parameters
         ----------
@@ -171,23 +252,14 @@ class SEIRModel(object):
         initial_conditions: list
             Values of S, E, I and R at the first day.
         loss_fn : str, optional
-            Loss function is `mse` by default, choose between `mse`, `rmse` and `msle`.
+            Loss function.
                 
         Returns
         -------
         float
-            Loss between the actual and predicted value of confirmed and recovered individuals.
+            Loss between the actual and predicted value of confirmed
+            and recovered individuals.
         """
-        if loss_fn == 'mse':
-            loss_fn = mse
-        elif loss_fn == 'rmse':
-            loss_fn = rmse
-        elif loss_fn == 'msle':
-            loss_fn = msle
-        elif loss_fn == 'mae':
-            loss_fn = mae
-        else:
-            raise ValueError('Given loss function is not supported.')
         size = active_cases.size
         sol = solve_ivp(
             fun=_SEIR, 
@@ -198,14 +270,16 @@ class SEIRModel(object):
             t_eval=np.arange(0, size, 1), 
             vectorized=True,
         )
-        return loss_fn(sol.y[2], active_cases) + loss_fn(sol.y[3], removed_cases)
+        return loss_fn(sol.y[2], active_cases) \
+               + loss_fn(sol.y[3], removed_cases)
 
 
-class SEIRDModel(object):
-    """SEIR model class."""
+class SEIRDModel(CompartmentalModel):
+    """SEIRD model class."""
+
     def __init__(self):
         """Constructor."""
-        pass
+        super().__init__()
 
     def fit(
         self, 
@@ -213,9 +287,8 @@ class SEIRDModel(object):
         recovered_cases, 
         death_cases, 
         initial_conditions, 
-        initial_guess=[0.5, 0.1, 0.01, 0.01], 
-        loss_fn='mse', 
-    ):
+        initial_guess=[0.5, 0.1, 0.01, 0.01],
+        ):
         """Fit SEIRD model.
         
         Parameters
@@ -229,14 +302,14 @@ class SEIRDModel(object):
         initial_conditions : list
             Values of S, E, I, R and D at the first day.
         initial_guess : list, optional
-            Array of real elements by means of possible values of independent variables.
-        loss_fn : str, optional
-            Loss function is `mse` by default.
+            Array of real elements by means of possible values of
+            independent variables.
         
         Returns
         -------
         tuple
-            Fitted epidemiological parameters: beta, alpha, gamma and mu rate.
+            Fitted epidemiological parameters: beta, alpha, gamma and
+            mu rate.
         list
             Loss values during the optimization procedure.
         """
@@ -244,34 +317,46 @@ class SEIRDModel(object):
         def print_loss(p):
             """Optimizer callback."""
             loss.append(
-                SEIRDModel._loss(p, active_cases, recovered_cases, death_cases, initial_conditions)
+                SEIRDModel._loss(
+                    p,
+                    active_cases,
+                    recovered_cases,
+                    death_cases,
+                    initial_conditions,
+                    self.loss_fn
+                    )
             )
             
         self.y0 = initial_conditions
         opt = minimize(
             fun=SEIRDModel._loss, 
             x0=initial_guess,
-            args=(active_cases, recovered_cases, death_cases, self.y0, loss_fn),
+            args=(
+                active_cases, recovered_cases, death_cases, self.y0, self.loss_fn
+                ),
             method='L-BFGS-B',
             bounds=[(1.e-6, 10.), (1.e-6, 10.), (1.e-6, 10.), (1.e-6, 10.),],
-            options={'disp': True, 'maxiter': 1000, 'ftol': 1.e-21, 'gtol': 1.e-31},
+            options={
+                'disp': True, 'maxiter': 1000, 'ftol': 1.e-21, 'gtol': 1.e-31
+                },
             callback=print_loss,
         )
         self.beta, self.alpha, self.gamma, self.mu = opt.x
         return (self.beta, self.alpha, self.gamma, self.mu), loss
 
     def predict(self, n_days):
-        """Forecast S, E, I and R based on the fitted epidemiological parameters.
+        """Forecast S, E, I and R based on the fitted epidemiological
+        parameters.
         
         Parameters
         ----------
         n_days : int
-            Number of days in future.
+            Number of days for which the simulation will be performed.
             
         Returns
         -------
         tuple
-            S, E, I and R values forecast for n_days in future.
+            S, E, I, R and D simulated values.
         """
         sol = solve_ivp(
             fun=_SEIRD, 
@@ -282,11 +367,22 @@ class SEIRDModel(object):
             t_eval=np.arange(0, n_days, 1), 
             vectorized=True,
         )
+        # TO-DO: implement sensitivity analysis
+        if self.sensitivity:
+            pass
         return (sol.y[0], sol.y[1], sol.y[2], sol.y[3], sol.y[4])
     
     @staticmethod
-    def _loss(params, active_cases, recovered_cases, death_cases, initial_conditions, loss_fn='mse'):
-        """Calculate and return the loss function between actual and predicted values.
+    def _loss(
+        params,
+        active_cases,
+        recovered_cases,
+        death_cases,
+        initial_conditions,
+        loss_fn,
+        ):
+        """Calculate and return the loss function between actual and
+        predicted values.
         
         Parameters
         ----------
@@ -301,23 +397,15 @@ class SEIRDModel(object):
         initial_conditions : list
             Values of S, E, I, R and D at the first day.
         loss_fn : str, optional
-            Loss function is `mse` by default, choose between `mse`, `rmse` and `msle`.
+            Loss function is `mse` by default, choose between `mse`,
+            `rmse` and `msle`.
         
         Returns
         -------
         float
-            Loss between the actual and predicted value of confirmed, recovered and deceased individuals.
+            Loss between the actual and predicted value of confirmed,
+            recovered and deceased individuals.
         """
-        if loss_fn == 'mse':
-            loss_fn = mse
-        elif loss_fn == 'rmse':
-            loss_fn = rmse
-        elif loss_fn == 'msle':
-            loss_fn = msle
-        elif loss_fn == 'mae':
-            loss_fn = mae
-        else:
-            raise ValueError('Given loss function is not supported.')
         size = active_cases.size
         sol = solve_ivp(
             fun=_SEIRD, 
@@ -328,4 +416,6 @@ class SEIRDModel(object):
             t_eval=np.arange(0, size, 1), 
             vectorized=True,
         )
-        return loss_fn(sol.y[2], active_cases) + loss_fn(sol.y[3], recovered_cases) + loss_fn(sol.y[4], death_cases)
+        return loss_fn(sol.y[2], active_cases) \
+               + loss_fn(sol.y[3], recovered_cases) \
+               + loss_fn(sol.y[4], death_cases)
