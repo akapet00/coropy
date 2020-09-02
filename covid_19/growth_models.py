8816a4854cad77d3120f8455a220c2bd7db81bd0
antelk <alojic00@fesb.hr>
2020-09-02 15:51:17 +0200

    in progress: ci using sensitivity and specificity

--- covid_19/growth_models.py
+++ covid_19/growth_models.py
@@ -1,3 +1,5 @@
+import warnings
+
 import numpy as np 
 from scipy.optimize import curve_fit 
 
@@ -89,16 +91,23 @@ class GrowthCOVIDModel(object):
             calculated using sensitivity as a valid accuracy measure.
             If confidence_interval flag is set to True but no kwargs
             (or just a single keyword argument) are given the
-            `confidence_interval` will be calulated assuming there are
+            confidence interval will be calulated assuming there are
             no intrinsic errors on measured data.
             sensitivity : float
                 Measure of the proportion of positives that are
                 correctly identified (e.g., the percentage of sick
                 people who are correctly identified as being infected).
+            specificity : float
+                Measure of the proportion of negatives that are 
+                correctly identified (e.g., the percentage of healthy
+                people who are correctly identified as not infected).
             ci_level : int
                 Confidence interval level. Supported values are 90, 95,
                 98 and 99 using the standard normal distribution as the
                 critical value.
+            daily_tests : numpy.ndarray
+                Tests performed daily. Size of an array should match
+                the size of confirmed cases while fitting growth model.
         """
         try:
             self.function = _growth_model_dispatcher[function]
@@ -106,21 +115,42 @@ class GrowthCOVIDModel(object):
             raise ValueError('Try `exponential` or `logistic` growth models.')
         self.normalize = normalize
         self.ci = confidence_interval
-        if len(kwargs) == 2: 
+        if kwargs and len(kwargs)==4: 
             for kw in kwargs.keys():
                 if kw == 'sensitivity':
                     assert isinstance(kwargs[kw], (float, )), \
                         'Sensitivity has to be a floating point number.'
                     self.sensitivity = kwargs[kw]
+                elif kw == 'specificity':
+                    assert isinstance(kwargs[kw], (float, )), \
+                        'Specificity has to be a floating point number.'
+                    self.specificity = kwargs[kw]
                 elif kw == 'ci_level':
                     assert kwargs[kw] in [90, 95, 98, 99], \
                         'Supported confidence interval levels are 90, 95, 98 \
                         and 99.'
                     self.ci_level = kwargs[kw]
+                elif kw == 'daily_tests':
+                    self.daily_tests = kwargs[kw]
                 else:
-                    raise ValueError('No such argument is supported.')
+                    self.sensitivity = None
+                    self.specificity = None
+                    self.ci_level = None
+                    self.daily_tests = None
+        elif kwargs and len(kwargs)!=4:
+            warnings.warn(
+                'One or more keyword arguments is invalid.'
+                'Confidence interval will be calulated assuming there are no '
+                'intrinsic errors on measured data.')
+            self.sensitivity = None
+            self.specificity = None
+            self.ci_level = None
+            self.daily_tests = None
         else:
-            self.sensitivity, self.ci_level = None, None
+            self.sensitivity = None
+            self.specificity = None
+            self.ci_level = None
+            self.daily_tests = None
                     
     def fit(self, confirmed_cases):
         """Fit the data to the growth function.
@@ -148,37 +178,32 @@ class GrowthCOVIDModel(object):
         x = np.arange(confirmed_cases.size)
         self.popt, self.pcov = curve_fit(self.function, x, y)
         fitted = self.function(x, *self.popt) 
-        if self.ci:
-            if self.sensitivity and self.ci_level:
-                raise NotImplementedError('TO-DO: implement sensitivity and \
-specificity based CI.')
-                # std_sensitivity_err = np.sqrt(
-                #     np.divide(
-                #         (1 - self.sensitivity) * self.sensitivity, 
-                #         y, 
-                #         out=np.zeros_like(y), 
-                #         where=y!=0,
-                #         )
-                # )
-                # sensitivity_ci = _ci_dispatcher[self.ci_level] \
-                #                  * std_sensitivity_err
-                # lower_bound = (self.sensitivity - sensitivity_ci) * fitted
-                # upper_bound = (self.sensitivity + sensitivity_ci) * fitted
-            else:
-                self.perr = np.sqrt(np.diag(self.pcov))
-                lower_bound = self.function(x, *(self.popt - self.perr))
-                upper_bound = self.function(x, *(self.popt + self.perr))
-        if self.normalize: 
-            fitted = restore(fitted, confirmed_cases)
-            if self.ci:
-                lower_bound = restore(lower_bound, confirmed_cases)
-                upper_bound = restore(upper_bound, confirmed_cases)
-        if self.ci:
+        if self.ci and not self.sensitivity:
+            self.perr = np.sqrt(np.diag(self.pcov))
+            lower_bound = self.function(x, *(self.popt - self.perr))
+            upper_bound = self.function(x, *(self.popt + self.perr))
+            if self.normalize: 
+                fitted = restore(fitted, self.confirmed_cases)
+                lower_bound = restore(lower_bound, self.confirmed_cases)
+                upper_bound = restore(upper_bound, self.confirmed_cases)
             fitted = np.r_[
                 lower_bound.reshape(1, -1), 
                 fitted.reshape(1, -1), 
                 upper_bound.reshape(1, -1),
-                ]
+            ]
+        elif (self.ci and self.sensitivity and self.specificity and 
+                self.ci_level and self.daily_tests is not None):
+            positives = np.diff(
+                np.concatenate((np.array([0]), self.confirmed_cases)))
+            fitted = self.calculate_ci(
+                self.sensitivity, 
+                self.specificity, 
+                positives, 
+                self.daily_tests, 
+                self.ci_level
+            )
+        elif not self.ci and self.normalize:
+            fitted = restore(fitted, self.confirmed_cases)
         return x, fitted
 
     def predict(self, n_days):
@@ -201,38 +226,100 @@ specificity based CI.')
         size = self.confirmed_cases.size
         x_future = np.arange(size-1, size+n_days)
         predicted = self.function(x_future, *self.popt)
-        if self.ci:
-            if self.sensitivity and self.ci_level:
-                raise NotImplementedError('TO-DO: implement sensitivity and \
-specificity based CI.')
-                # std_sensitivity_err = np.sqrt(
-                #     np.divide(
-                #         (1 - self.sensitivity) * self.sensitivity, 
-                #         predicted, 
-                #         out=np.zeros_like(predicted), 
-                #         where=predicted!=0,
-                #         )
-                # )
-                # sensitivity_ci = _ci_dispatcher[self.ci_level] \
-                #                  * std_sensitivity_err
-                # lower_bound = (self.sensitivity - sensitivity_ci) * predicted
-                # upper_bound = (self.sensitivity + sensitivity_ci) * predicted
-            else:
-                lower_bound = self.function(
-                    x_future, *(self.popt - self.perr)
-                    )
-                upper_bound = self.function(
-                    x_future, *(self.popt + self.perr)
-                    )
-        if self.normalize: 
-            predicted = restore(predicted, self.confirmed_cases)
-            if self.ci:
+        if self.ci and not self.sensitivity:
+            self.perr = np.sqrt(np.diag(self.pcov))
+            lower_bound = self.function(x_future, *(self.popt - self.perr))
+            upper_bound = self.function(x_future, *(self.popt + self.perr))
+            if self.normalize: 
+                predicted = restore(predicted, self.confirmed_cases)
                 lower_bound = restore(lower_bound, self.confirmed_cases)
                 upper_bound = restore(upper_bound, self.confirmed_cases)
-        if self.ci:
             predicted = np.r_[
                 lower_bound.reshape(1, -1), 
                 predicted.reshape(1, -1), 
                 upper_bound.reshape(1, -1),
-                ]
+            ]
+        elif (self.ci and self.sensitivity and self.specificity and 
+                self.ci_level and self.daily_tests is not None):
+            positives = np.diff(
+                np.concatenate((np.array([0]), predicted)))
+            predicted = self.calculate_ci(
+                self.sensitivity, 
+                self.specificity, 
+                positives, 
+                self.daily_tests, 
+                self.ci_level
+            )
+        elif not self.ci and self.normalize:
+            predicted = restore(predicted, self.confirmed_cases)
         return x_future, predicted
+
+    @staticmethod
+    def calculate_ci(
+        sensitivity, 
+        specificity, 
+        positives,
+        daily_tests,
+        ci_level,
+        ):
+        """Return 2-d array with 3 rows, first row is the lower
+        confidence interval bound, the second row is the fitted data
+        and the last row is the upper confidence interval bound.
+        Parameters
+        ----------
+        sensitivity : float or numpy.ndarray
+            Test sensitivity. If the sensitivity is different in
+            different intervals, it should be stored in the array-like
+            format where the length is the same as the length of the
+            data.
+        specificity : float or numpy.ndarray
+            Test specificity. If the sensitivity is different in
+            different intervals, it should be stored in the array-like
+            format where the length is the same as the length of the
+            data.
+        positives : numpy.ndarray
+            Daily number of new confirmed positive infections.       
+        daily_tests : numpy.ndarray
+            Daily number of tests.
+        ci_level : int
+            Confidence interval level. Supported values are 90, 95,
+            98 and 99 using the standard normal distribution as the
+            critical value.
+        Returns
+        -------
+        numpy.ndarray
+            Array of shape (3, n) where n is the duration of modeled
+            epidemics in days. The first row is the lower CI bound, the
+            second row is the fitted data and the third row is upper CI
+            bound.
+        """
+        std_sensitivity_err = np.sqrt(np.divide(
+            (1 - sensitivity) * sensitivity, 
+            positives, 
+            out=np.zeros(positives.shape, dtype=float), 
+            where=positives!=0,
+        ))
+        sensitivity_ci = _ci_dispatcher[ci_level] * std_sensitivity_err
+        lower_bound_sensitivity = np.abs(sensitivity - sensitivity_ci)
+        lower_bound_true_positives = lower_bound_sensitivity * positives
+        lower_bound_cumulative_cases = np.cumsum(lower_bound_true_positives)
+        
+        negatives = daily_tests - positives
+        std_specificity_err = np.sqrt(np.divide(
+            (1 - specificity) * specificity,
+            negatives,
+            out=np.zeros(negatives.shape, dtype=float), 
+            where=negatives!=0,
+        ))
+        specificity_ci = _ci_dispatcher[ci_level] * std_specificity_err
+        upper_bound_specificity = np.abs(specificity + specificity_ci)
+        upper_bound_true_negatives = upper_bound_specificity * negatives
+        upper_bound_false_negatives = negatives - upper_bound_true_negatives
+        upper_bound_true_positives = upper_bound_false_negatives + positives
+        upper_bound_cumulative_cases = np.cumsum(upper_bound_true_positives)
+
+        return np.r_[
+            lower_bound_cumulative_cases.reshape(1, -1),
+            np.cumsum(positives).reshape(1, -1),
+            upper_bound_cumulative_cases.reshape(1, -1),
+        ]
