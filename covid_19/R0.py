e8c4b7822f854a6026d8c9b56c05685253dfb8e7
antelk <alojic00@fesb.hr>
2020-09-06 00:52:32 +0200

    add docstrings

--- covid_19/R0.py
+++ covid_19/R0.py
@@ -2,6 +2,7 @@
 # EXPERIMENTAL #
 ################
 
+import warnings
 
 import numpy as np
 
@@ -9,11 +10,36 @@ from covid_19.utils import moving_average
 
 
 def uncertainty_quantification(
-    sensitivity, 
-    specificity, 
-    confirmed_cases,
-    daily_tests,
-    ):
+    sensitivity, specificity, confirmed_cases, daily_tests):
+    """Return the lower and the upper bound scaler for the reproduction
+    number in time in the 95% confidence interval.
+    
+    Parameters
+    ----------
+    sensitivity : float or numpy.ndarray
+        Test sensitivity. If the sensitivity is different in
+        different intervals, it should be stored in the array-like
+        format where the length is the same as the length of the
+        data.
+    specificity : float or numpy.ndarray
+        Test specificity. If the sensitivity is different in
+        different intervals, it should be stored in the array-like
+        format where the length is the same as the length of the
+        data.
+    confirmed_cases : numpy.ndarray
+        The total number of confirmed infected COVID-19 cases per day.     
+    daily_tests : numpy.ndarray
+        Daily number of tests.
+    
+    Returns
+    -------
+    lower_bound_scaler : numpy.ndarray
+        Array of shape (n, ) where n is the duration of modeled
+        epidemics in days with values of lower CI bound scaler. 
+    upper_bound_scaler : numpy.ndarray
+        Array of shape (n, ) where n is the duration of modeled
+        epidemics in days with values of upper CI bound scaler. 
+    """
     positives = np.diff(confirmed_cases)
     std_sensitivity_err = np.sqrt(np.divide(
         (1 - sensitivity) * sensitivity, 
@@ -57,6 +83,33 @@ def uncertainty_quantification(
 
 
 def estimate(confirmed_cases, averaging_period=7, testing_delay=5):
+    """Return the time series estimate for reproduction number based on
+    the number of the total number of positive confirmed cases.
+
+    Parameters
+    ----------
+    averaging_period : int
+        Moving average window.
+    testing_delay : int
+        Number of days from the first symptom to confirmed infection.
+
+    Returns
+    -------
+    numpy.ndarray
+        Reproduction number values with the (n, ) shape where n is the
+        total accumulated delay consisted of `averagin_period` and
+        `testing_delay`.
+    """
+    if not isinstance(averaging_period, (int, )):
+        averaging_period = int(averaging_period)
+        warnings.warn(
+            '`averaging_period` should be a positive integer.'
+            'Automatic conversion float -> int is applied.')
+    if not isinstance(testing_delay, (int, )):
+        averaging_period = int(averaging_period)
+        warnings.warn(
+            '`testing_delay` should be a positive integer.'
+            'Automatic conversion float -> int is applied.')
     confirmed_cases_bias = np.r_[0, confirmed_cases]
     new_daily_cases = np.diff(confirmed_cases_bias)
     smoothed_new_daily_cases = moving_average(new_daily_cases, averaging_period)
@@ -65,4 +118,4 @@ def estimate(confirmed_cases, averaging_period=7, testing_delay=5):
         smoothed_new_daily_cases[:-testing_delay],
         out=np.zeros(smoothed_new_daily_cases[:-testing_delay].size, dtype=float),
         where=smoothed_new_daily_cases[:-testing_delay]!=0)
-    return R, smoothed_new_daily_cases
+    return R
