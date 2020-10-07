b4cb10a3b0dc8cb101a4737ff0ee5847710518ec
antelk <alojic00@fesb.hr>
2020-10-07 11:34:25 +0200

    fix bug

--- data/CRO/scrape_koronavirushr.py
+++ data/CRO/scrape_koronavirushr.py
@@ -2,17 +2,19 @@ import requests
 
 
 URL = 'https://koronavirus.hr'
-TARGET = 'testirano je'
+TARGETS = ['testirano je', 'testirane su']
 
 
 def main():
     x = requests.get(URL)
-    target_idx = x.text.find(TARGET)
-    daily_tests_idx = (target_idx + len(TARGET), target_idx + len(TARGET) + 6)
-    daily_tests_nmb = int(
-        x.text[daily_tests_idx[0]:daily_tests_idx[1]].replace('.', ''))
-    with open('tests.dat', 'a') as f:
-            f.write(f'\n{daily_tests_nmb}')
+    for TARGET in TARGETS:
+        target_idx = x.text.find(TARGET)
+        if target_idx != -1:
+            daily_tests_idx = (target_idx + len(TARGET), target_idx + len(TARGET) + 6)
+            daily_tests_nmb = int(
+                x.text[daily_tests_idx[0]:daily_tests_idx[1]].replace('.', ''))
+            with open('test.dat', 'a') as f:
+                f.write(f'\n{daily_tests_nmb}')
 
 
 if __name__ == "__main__":
